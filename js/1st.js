9f3ecce1a69da3b0d76f6cde6a527b56598bb254
unknown <tamuna@saba.com.ge>
2015-06-05 19:14:44 +0400

    done

--- js/1st.js
+++ js/1st.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
 
+    var headers = ["Cop sentenced for shooting his ex-wife", "Policeman who killed his domestic partner pleaded temporary insanity", "After a woman meets her ex-husband she’s killed", "Nine shots below the waist kills the women", "Confession after the murder"]
     $(".parts p:not(:first-child)").click(function () {
 
         var choosedItemId = $(this).data("id")
@@ -8,5 +9,11 @@
     $(".start_reading").click(function () {
 
         $('html, body').animate({ scrollTop: $('#part1').offset().top }, 'slow');
+    });
+
+    $(".parts p:not(:first-child)").mouseenter(function () {
+     
+        $(".title").text("")
+        $(".title").text(headers[$(this).text() - 1])
     });
 })
